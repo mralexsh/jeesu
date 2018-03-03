05f71f2af402273b0268d6b56b52fc962dfbdb48
has <mralexsh@mail.ru>
2018-03-03 18:45:31 +0700

    base logic

---
+++ sensors/src/main/java/udo/tusur/ru/sensors/PseudoSensor.java
@@ -0,0 +1,50 @@
+package udo.tusur.ru.sensors;
+
+import java.util.Date;
+
+public class PseudoSensor implements Sensor, SensorEmulator {
+
+	private int value;
+	private int type;
+	private String id;
+	private long timeStamp;
+	private Date date;
+	private EmulationStrategy emulattionStrategy;
+	public PseudoSensor() {
+		this.date = new Date();
+	}
+	public void setValue(int value) {
+		this.value = value;
+		this.timeStamp = this.date.getTime();
+	}
+	public String getId() {
+		return this.id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getValue() {
+		return this.value;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public int getType() {
+		return this.type;
+	}
+	public long getTimeStamp() {
+		return this.timeStamp;
+	}
+	public EmulationStrategy getEmulattionStrategy() {
+		return emulattionStrategy;
+	}
+	public void setEmulattionStrategy(EmulationStrategy emulattionStrategy) {
+		this.emulattionStrategy = emulattionStrategy;
+	}
+	public void doEmulate() {
+		if (this.emulattionStrategy != null) {
+			this.emulattionStrategy.doEmulate(this);	
+		}
+	}
+
+}
---
+++ sensors/src/main/java/udo/tusur/ru/sensors/SensorEmulationRuntime.java
@@ -0,0 +1,55 @@
+package udo.tusur.ru.sensors;
+
+import java.util.List;
+
+public class SensorEmulationRuntime extends Thread implements SensorObserver {
+	private List<PseudoSensor> pseudoSensors;
+	private List<SensorObservable> listeners;
+	private int refreshTicks;
+	SensorEmulationRuntime() {
+		this.refreshTicks = 50;
+	}
+	public List<PseudoSensor> getPseudoSensors() {
+		return pseudoSensors;
+	}
+	public void setPseudoSensors(List<PseudoSensor> pseudoSensors) {
+		this.pseudoSensors = pseudoSensors;
+	}
+	public List<SensorObservable> getListeners() {
+		return listeners;
+	}
+	public void setListeners(List<SensorObservable> listeners) {
+		this.listeners = listeners;
+	}
+	public int getRefreshTicks() {
+		return refreshTicks;
+	}
+	public void setRefreshTicks(int refreshTicks) {
+		this.refreshTicks = refreshTicks;
+	}
+	public void run() {
+		while(true) {
+			try {
+				for (PseudoSensor sensor: this.pseudoSensors) {
+					sensor.doEmulate();
+				}
+				sleep(this.refreshTicks);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public void register(SensorObservable o) {
+		this.listeners.add(o);
+		
+	}
+	public void unregister(SensorObservable o) {
+		this.listeners.remove(o);
+		
+	}
+	public void notifyObservables() {
+		for (SensorObservable item: this.listeners) {
+			item.handle(this);
+		}
+	}
+}
