c9fe6a462231fe6d3f1c3835b5b7f15d7d93faa6
mralexsh <Shimonchuk@gmail.com>
2020-02-23 09:37:19 +0700

    jackson databind removed. Camel version fixed

--- sensors/src/main/java/ru/tusur/udo/sensors/SensorsRoutes.java
+++ sensors/src/main/java/ru/tusur/udo/sensors/SensorsRoutes.java
@@ -3,56 +3,46 @@ package ru.tusur.udo.sensors;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 
 public class SensorsRoutes extends RouteBuilder {
 
-	@Value("${polling.interval}")
-	private int pollingInterval;
-	
-	@Value("${app.server.endpoint}")
-	private String appServerEndpoint;
-	
-	@Autowired
-	private SensorsRuntimeProcessor sensorsRuntimeProcessor;
-	
-	@Autowired
-	private EnrichDataProcessor enrichDataProcessor;
-	
-	@Autowired
-	private JsonConvertProcessor jsonConvertorProcessor;
-	
-	@Override
-	public void configure() throws Exception {
-			
-		from("timer://sensorTimer?period=" + pollingInterval)
-			.process(sensorsRuntimeProcessor)			
-			.to("direct:enrichDataRoute");	
-						
-		from("direct:enrichDataRoute")
-		.process(enrichDataProcessor)
-		.to("direct:jsonRoute");
-		
-		from("direct:jsonRoute")
-		.process(jsonConvertorProcessor)
-		.to("direct:sendRoute");
+    @Value("${polling.interval}")
+    private int pollingInterval;
 
-		
-		from("direct:sendRoute")
-		.setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
-		.doTry()
-		.log("${body}")
-		.to("http4:" + appServerEndpoint)		
-		.doCatch(Exception.class)		
-		.log(LoggingLevel.ERROR ,"Не согу соединиться с сервером");
-		
-		
-		
-		
-	}
+    @Value("${app.server.endpoint}")
+    private String appServerEndpoint;
+
+    @Autowired
+    private SensorsRuntimeProcessor sensorsRuntimeProcessor;
+
+    @Autowired
+    private EnrichDataProcessor enrichDataProcessor;
+
+    @Override
+    public void configure() throws Exception {
+
+        from("timer://sensorTimer?period=" + pollingInterval)
+                .process(sensorsRuntimeProcessor)
+                .to("direct:enrichDataRoute");
+
+        from("direct:enrichDataRoute")
+                .process(enrichDataProcessor)
+                .marshal().json(JsonLibrary.Jackson, SensorsJSONSchema.class)
+                .to("direct:sendRoute");
+
+        from("direct:sendRoute")
+                .setHeader(Exchange.HTTP_METHOD, constant(HttpMethods.POST))
+                .doTry()
+                .log("${body}")
+                .to("http4:" + appServerEndpoint)
+                .doCatch(Exception.class)
+                .log(LoggingLevel.ERROR, "Can't connect to wildfly server");
+
+    }
 
 }
